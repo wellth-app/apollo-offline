b55317a7fa652fdffdb66cff324161cb2101bb3f
Justin Makaila <justin@wellthapp.com>
2019-03-26 16:18:23 -0400

    Added new utils for determining if something is a uuid, as well as a helper for parsing graphql documents.

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,1 +1,12 @@
+import { DocumentNode, OperationDefinitionNode, FieldNode } from "graphql";
+import { resultKeyNameFromField } from "apollo-utilities";
+
 export { default as rootLogger } from "./logger";
+
+export const isUuid = (val: any) =>
+  typeof val === "string" &&
+  val.match(/[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}/i);
+
+export const getOperationFieldName = (operation: DocumentNode): string =>
+  resultKeyNameFromField((operation.definitions[0] as OperationDefinitionNode)
+    .selectionSet.selections[0] as FieldNode);
