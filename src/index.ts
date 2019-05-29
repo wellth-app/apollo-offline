dd74be73c631566f473f9b990da6cb495d5cc06d
Justin Makaila <justin@wellthapp.com>
2019-05-29 11:04:58 -0400

    Clean up root exports

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,3 @@
 export * from "./client";
 export { default } from "./client";
 export { default as OfflineLink } from "./links/offline";
-export { REHYDRATE_STORE } from "./actions/rehydrateStore";
-export { rootLogger } from "./utils";
